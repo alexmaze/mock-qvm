4a8f005467c52c809ff551685932e837528a79c3
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-11-08 17:12:06 +0800

    add list instance

--- src/api/instance/index.js
+++ src/api/instance/index.js
@@ -3,7 +3,7 @@ const Router = require("koa-router")
 const router = new Router()
 
 router.get("/", ctx => {
-  ctx.body = "instance list"
+  ctx.body = require("./list.json")
 })
 
 module.exports = router
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(async (ctx, next) => {
 })
 
 // regist routes
-router.use("/instance", require("./apis/instance").routes())
+router.prefix("/api").use("/instance", require("./api/instance").routes())
 
 app.use(router.routes()).listen(3000)
 console.log("server running @:3000")
